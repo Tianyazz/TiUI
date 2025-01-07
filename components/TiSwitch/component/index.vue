2dd8dea7dcdafcbe92dc3f390dd7f85fb919f401
tianya <tianya900625@163.com>
2025-01-07 15:32:37 +0800

    :sparkles: 开关小组件

---
+++ components/TiSwitch/component/index.vue
@@ -0,0 +1,80 @@
+<!-- 开关 2024-08-16 END -->
+<template>
+  <view
+    :class="['switch', { active: modelValue }]"
+    @click="toggle"
+  >
+    <view class="i"></view>
+    <label>{{ modelValue ? onLabel : offLabel }}</label>
+  </view>
+</template>
+
+<script setup name="Switch">
+const props = defineProps({
+  modelValue: {
+    type: [Number, Boolean],
+    default: false
+  },
+  onLabel: {
+    type: String,
+    default: '加急'
+  },
+  offLabel: {
+    type: String,
+    default: '普通'
+  },
+  type: {
+    type: String,
+    default: 'boolean'
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+function toggle() {
+  let val = props.modelValue
+  if (typeof val === 'number') {
+    val = val ? 0 : 1
+  } else {
+    val = !props.modelValue
+  }
+  emit('update:modelValue', val)
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/static/css/global.scss';
+.switch {
+  @include flexs;
+  @include align-items;
+  height: 80rpx;
+  .i {
+    @include flexs;
+    @include align-items;
+    width: 100rpx;
+    height: 50rpx;
+    margin-right: 20rpx;
+    border-radius: 25rpx;
+    background: $uni-color-blue;
+    @include transition;
+    &::before {
+      content: "";
+      display: block;
+      width: 42rpx;
+      height: 42rpx;
+      @include translate(4rpx, 0);
+      background: $uni-color-white;
+      border-radius: 25rpx;
+      @include transition(all, .3s, ease-out);
+    }
+  }
+  &.active {
+    .i {
+      box-shadow: 2rpx 4rpx 10rpx 0 rgba(150, 116, 74, .4) inset;
+      background: $uni-color-red;
+      &:before {
+        @include translate(54rpx, 0);
+      }
+    }
+  }
+}
+</style>
---
+++ components/TiSwitch/index.ts
@@ -0,0 +1,6 @@
+import { withNoopInstall } from '../../utils/vue'
+import Switch from './src/index.vue'
+
+const TiSwitch = withNoopInstall(Switch)
+
+export default TiSwitch
